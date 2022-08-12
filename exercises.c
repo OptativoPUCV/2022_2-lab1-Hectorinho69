789dd6434bc5f049a1551540c360b0b95978f1da
Replit user <>
2022-08-12 15:24:27 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -58,8 +58,8 @@ typedef struct {
 
 Persona* crearPersona(char nombre[], char rut[], int edad) {
   Persona* p[3];
-  p->nombre = nombre[];
-  p->rut = rut[];
+  p->nombre = nombre;
+  p->rut = rut;
   p->edad = edad;
    return NULL;
 }
